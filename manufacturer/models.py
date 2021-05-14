77914448220c41eb8aa0ab1177a867836ea98822
trudzer <trudzeredits@gmail.com>
2021-05-14 19:21:07 +0000

    adfsdg
    gdf

--- manufacturer/models.py
+++ manufacturer/models.py
@@ -6,6 +6,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255)
     year_of_start = models.DateTimeField()
     logo = models.CharField(max_length=9999, blank=True)
+    homepage = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.name
